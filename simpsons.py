2c3e3fc0762893c8f3722c22f258d5fb4e2901bc
fridchikn24 <10899472+fridchikn24@users.noreply.github.com>
2022-09-05 13:58:33 -0500

    added random forests and decision trees

--- simpsons.py
+++ simpsons.py
@@ -85,7 +85,8 @@ from pathlib import Path
 from sklearn.neighbors import KNeighborsRegressor
 
 
-
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.ensemble import RandomForestClassifier
 #import os
 #os.makedirs("/content/drive/MyDrive/teaching_fall_2021/ml_fall_2021/HW_Assignments/HW6/saved_models")
 #!ls
@@ -136,3 +137,187 @@ grid_knn_1 = GridSearchCV(SpringField_pipeline,param_grid_1,cv=5,return_train_sc
 grid_knn_1.fit(x_train,y_train)
 
 grid_knn_1.best_params_
+
+def plot_learning_curve(estimator, title, X, y, axes=None, ylim=None, cv=None,
+                        n_jobs=None, train_sizes=np.linspace(.1, 1.0, 5)):
+    """
+    Generate 2 plots: the test and training learning curve, the training
+    samples vs fit times curve.
+
+    Parameters
+    ----------
+    estimator : estimator instance
+        An estimator instance implementing `fit` and `predict` methods which
+        will be cloned for each validation.
+
+    title : str
+        Title for the chart.
+
+    X : array-like of shape (n_samples, n_features)
+        Training vector, where ``n_samples`` is the number of samples and
+        ``n_features`` is the number of features.
+
+    y : array-like of shape (n_samples) or (n_samples, n_features)
+        Target relative to ``X`` for classification or regression;
+        None for unsupervised learning.
+
+    axes : array-like of shape (3,), default=None
+        Axes to use for plotting the curves.
+
+    ylim : tuple of shape (2,), default=None
+        Defines minimum and maximum y-values plotted, e.g. (ymin, ymax).
+
+    cv : int, cross-validation generator or an iterable, default=None
+        Determines the cross-validation splitting strategy.
+        Possible inputs for cv are:
+
+          - None, to use the default 5-fold cross-validation,
+          - integer, to specify the number of folds.
+          - :term:`CV splitter`,
+          - An iterable yielding (train, test) splits as arrays of indices.
+
+        For integer/None inputs, if ``y`` is binary or multiclass,
+        :class:`StratifiedKFold` used. If the estimator is not a classifier
+        or if ``y`` is neither binary nor multiclass, :class:`KFold` is used.
+
+        Refer :ref:`User Guide <cross_validation>` for the various
+        cross-validators that can be used here.
+
+    n_jobs : int or None, default=None
+        Number of jobs to run in parallel.
+        ``None`` means 1 unless in a :obj:`joblib.parallel_backend` context.
+        ``-1`` means using all processors. See :term:`Glossary <n_jobs>`
+        for more details.
+
+    train_sizes : array-like of shape (n_ticks,)
+        Relative or absolute numbers of training examples that will be used to
+        generate the learning curve. If the ``dtype`` is float, it is regarded
+        as a fraction of the maximum size of the training set (that is
+        determined by the selected validation method), i.e. it has to be within
+        (0, 1]. Otherwise it is interpreted as absolute sizes of the training
+        sets. Note that for classification the number of samples usually have
+        to be big enough to contain at least one sample from each class.
+        (default: np.linspace(0.1, 1.0, 5))
+    """
+    if axes is None:
+        _, axes = plt.subplots(1, 2, figsize=(10, 5))
+
+    axes[0].set_title(title)
+    if ylim is not None:
+        axes[0].set_ylim(*ylim)
+    axes[0].set_xlabel("Training examples")
+    axes[0].set_ylabel("Score")
+
+    train_sizes, train_scores, test_scores, fit_times, _ = \
+        learning_curve(estimator, X, y, cv=cv, n_jobs=n_jobs,
+                       train_sizes=train_sizes,
+                       return_times=True,
+                       random_state=123)
+    train_scores_mean = np.mean(train_scores, axis=1)
+    train_scores_std = np.std(train_scores, axis=1)
+    test_scores_mean = np.mean(test_scores, axis=1)
+    test_scores_std = np.std(test_scores, axis=1)
+    fit_times_mean = np.mean(fit_times, axis=1)
+    fit_times_std = np.std(fit_times, axis=1)
+
+    # Plot learning curve
+    axes[0].grid()
+    axes[0].fill_between(train_sizes, train_scores_mean - train_scores_std,
+                         train_scores_mean + train_scores_std, alpha=0.1,
+                         color="r")
+    axes[0].fill_between(train_sizes, test_scores_mean - test_scores_std,
+                         test_scores_mean + test_scores_std, alpha=0.1,
+                         color="g")
+    axes[0].plot(train_sizes, train_scores_mean, 'o-', color="r",
+                 label="Training score")
+    axes[0].plot(train_sizes, test_scores_mean, 'o-', color="g",
+                 label="Cross-validation score")
+    axes[0].legend(loc="best")
+
+    # Plot n_samples vs fit_times
+    axes[1].grid()
+    axes[1].plot(train_sizes, fit_times_mean, 'o-')
+    axes[1].fill_between(train_sizes, fit_times_mean - fit_times_std,
+                         fit_times_mean + fit_times_std, alpha=0.1)
+    axes[1].set_xlabel("Training examples")
+    axes[1].set_ylabel("fit_times")
+    axes[1].set_title("Scalability of the model")
+
+    return plt
+
+file_best_estimator_round1 = save_model_folder / 'knn_round1_best_estimator.pkl'
+file_complete_grid_round1 = save_model_folder / 'knn_round1_complete_grid.pkl'
+
+joblib.dump(grid_knn_1.best_estimator_, file_best_estimator_round1)
+joblib.dump(grid_knn_1, file_complete_grid_round1)
+
+loaded_best_estimator_round1 = joblib.load(file_best_estimator_round1)
+loaded_complete_grid_round1 = joblib.load(file_complete_grid_round1)
+
+plot_learning_curve(loaded_best_estimator_round1, 'Learning Curves KNN', x_train, y_train, n_jobs=-1)
+
+#let's check the train scores
+print(loaded_best_estimator_round1.score(x_train,y_train))
+
+#let's check the cross validation score
+print(loaded_complete_grid_round1.best_score_)
+
+#Making f2 scorer for Grid search CV
+from sklearn.metrics import fbeta_score, make_scorer
+ftwo_scorer = make_scorer(fbeta_score, beta=2)
+ftwo_scorer
+
+"""Decision Tree"""
+
+from scipy.stats import uniform, truncnorm, randint, loguniform
+
+dtree = DecisionTreeClassifier()
+
+dtree_param = {"max_depth": range(1,10),
+           "min_samples_split": range(2,10,1),
+           "max_leaf_nodes": range(2,6),
+              "splitter": ["best", "random"]}
+grid_dtree = GridSearchCV(dtree, dtree_param,cv=5, return_train_score=True, scoring=ftwo_scorer)
+grid_dtree.fit(x_train,y_train)
+
+grid_dtree.best_params_
+
+file_best_estimator_round2 = save_model_folder / 'dtree_best_estimator.pkl'
+file_complete_grid_round2 = save_model_folder / 'dtree1_complete_grid.pkl'
+
+joblib.dump(grid_dtree.best_estimator_, file_best_estimator_round2)
+joblib.dump(grid_dtree, file_complete_grid_round2)
+
+loaded_best_estimator_round2 = joblib.load(file_best_estimator_round2)
+loaded_complete_grid_round2 = joblib.load(file_complete_grid_round2)
+
+plot_learning_curve(loaded_best_estimator_round2, 'Learning Curves Dtree', x_train, y_train, n_jobs=-1)
+
+#let's check the train scores
+print(loaded_best_estimator_round2.score(x_train,y_train))
+
+#let's check the cross validation score
+print(loaded_complete_grid_round2.best_score_)
+
+from sklearn.ensemble import BaggingClassifier
+
+bag_clf = BaggingClassifier(
+    DecisionTreeClassifier(random_state=123), n_estimators=500,
+    max_samples=100, bootstrap=True, random_state=123,oob_score=True)
+bag_clf.fit(x_train, y_train)
+y_pred = bag_clf.predict(x_test)
+
+bag_clf.oob_score_
+
+from sklearn.metrics import accuracy_score
+print(accuracy_score(y_test, y_pred))
+
+grid_dtree.best_estimator_
+
+from sklearn.tree import plot_tree
+plot_tree(loaded_best_estimator_round2.fit(simpsons_vars, golden_age))
+
+tree_clf_param = grid_dtree.best_estimator_
+#tree_clf = DecisionTreeClassifier(tree_clf_param)
+
+
